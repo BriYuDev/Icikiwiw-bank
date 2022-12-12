2b642254014204ca91084d5e5ae2590b622f4a7f
BriYuDev <aesblox12@gmail.com>
2022-12-12 11:14:30 +0700

    new changes

--- src/components/business.jsx
+++ src/components/business.jsx
@@ -43,7 +43,10 @@ class Business extends Component {
   state = {};
   render() {
     return (
-      <section className={layout.section}>
+      <section
+        id='features'
+        className={layout.section}
+      >
         <div className={layout.sectionInfo}>
           <h2 className={styles.heading2}>Mamang And Icikiwir</h2>
           <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
